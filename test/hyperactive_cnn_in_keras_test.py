e419164a92959524c15357274df0eb7a4e8a6a54
TatsukiSerizawa <iputatsuki@gmail.com>
2019-08-01 23:46:17 +0900

    small test

--- test/hyperactive_cnn_in_keras_test.py
+++ test/hyperactive_cnn_in_keras_test.py
@@ -20,28 +20,31 @@ y_test = to_categorical(y_test)
 
 # this defines the structure of the model and the search space in each layer
 search_config = {
-    "keras.compile.0": {"loss": ["categorical_crossentropy"], "optimizer": ["adam"]},
-    "keras.fit.0": {"epochs": [20], "batch_size": [500], "verbose": [2]},
+    "keras.compile.0": {"loss": ["categorical_crossentropy"], "optimizer": ["SGD"]},
+    "keras.fit.0": {"epochs": [5], "batch_size": [500], "verbose": [2]},
     "keras.layers.Conv2D.1": {
-        "filters": [32, 64, 128],
-        "kernel_size": range(3, 4),
+        "filters": [32, 64],
+        #"kernel_size": range(3, 4),
+        "kernel_size": [3],
         "activation": ["relu"],
         "input_shape": [(28, 28, 1)],
     },
     "keras.layers.MaxPooling2D.2": {"pool_size": [(2, 2)]},
     "keras.layers.Conv2D.3": {
-        "filters": [16, 32, 64],
+        "filters": [16, 32],
         "kernel_size": [3],
         "activation": ["relu"],
     },
     "keras.layers.MaxPooling2D.4": {"pool_size": [(2, 2)]},
     "keras.layers.Flatten.5": {},
-    "keras.layers.Dense.6": {"units": range(30, 200, 10), "activation": ["softmax"]},
-    "keras.layers.Dropout.7": {"rate": list(np.arange(0.4, 0.8, 0.1))},
+    "keras.layers.Dense.6": {"units": 30, "activation": ["softmax"]},
+    #"keras.layers.Dense.6": {"units": range(30, 200, 10), "activation": ["softmax"]},
+    "keras.layers.Dropout.7": {"rate": 0.4},
+    #"keras.layers.Dropout.7": {"rate": list(np.arange(0.4, 0.8, 0.1))},
     "keras.layers.Dense.8": {"units": [10], "activation": ["softmax"]},
 }
 
-Optimizer = RandomSearchOptimizer(search_config, n_iter=20)
+Optimizer = RandomSearchOptimizer(search_config, n_iter=10)
 #Optimizer = ParticleSwarmOptimizer(search_config, n_iter=20)
 
 t1 = time.time()
